86bb1563a188b81932c9ac7a27b56eb5192fe818
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-05-19 07:19:41 -0700

    multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+
+    for key, value in new_dict:
+        new_dict[key] = value ** 2
+
+    return new_dict
