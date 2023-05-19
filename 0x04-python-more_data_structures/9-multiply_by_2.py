cadeac8b51024233bbd32f1d5738638f145b08dd
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-05-19 07:22:49 -0700

    fix logic

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,6 +4,6 @@ def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
 
     for key, value in new_dict.items():
-        new_dict[key] = value ** 2
+        new_dict[key] = value * 2
 
     return new_dict
