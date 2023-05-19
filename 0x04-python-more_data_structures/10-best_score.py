2dc4e4b1936b52b3080e4432f041dd27e8ee5fa0
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-05-19 07:36:52 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+
+    keys_list = list(a_dictionary.keys())
+
+    index = list(a_dictionary.values()).index(max(a_dictionary.values()))
+
+    return keys_list[index]
