3f4d5178223447c4daebfcd2e5dcb4da5f3f9de0
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-06-01 09:41:13 -0700

    add area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Module Square
+
+Classes:
+    Square - A class square
+"""
+
+
+class Square:
+    """
+    An class square
+
+    Args:
+        __size: the size of the square. should be an
+        int that's greater than 0
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
