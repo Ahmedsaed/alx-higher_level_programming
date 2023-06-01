f4110b7cb39cd22e910028b8e5ac61a5900f4c13
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-06-01 09:24:58 -0700

    square with private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module Square
+
+Classes:
+    Square - A class square
+"""
+
+
+class Square:
+    """
+    An class square
+
+    Attributes:
+        _size: private instance size
+
+    Args:
+        _size: the size of the square
+    """
+    def __init__(self, size):
+        self._size = size
