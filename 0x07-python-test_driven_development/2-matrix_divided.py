239dd682f42ae3a09f4695d4cc670a012ec22278
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-06-08 05:29:53 +0300

    update test cases for matrix devision

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -25,7 +25,6 @@ def matrix_divided(matrix, div):
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-
     elif int(div) == 0:
         raise ZeroDivisionError("division by zero")
 
