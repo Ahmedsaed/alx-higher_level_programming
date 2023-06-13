4fadbfb2a67ef9f9c00e3136c0fec144bd5ae020
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-06-14 01:48:21 +0300

    update code style

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,7 @@ import json
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+
 def main():
     """main function. it creates/overwrites add_item.json
     file with arguments"""
