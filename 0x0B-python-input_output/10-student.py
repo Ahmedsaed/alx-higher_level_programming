7191ddfe522b2537ca181b48ec1fde4b995fce3b
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-06-14 02:14:04 +0300

    Fix logix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,6 +13,6 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student instance"""
         if type(attrs) is list:
-            if all(e is str for e in attrs):
+            if all(type(e) is str for e in attrs):
                 return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
