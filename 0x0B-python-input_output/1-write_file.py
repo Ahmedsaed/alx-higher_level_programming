61535aec6683e271dce8e17d6428190c99328b67
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-06-14 01:13:35 +0300

    add docstring

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,5 +2,6 @@
 """write_file function"""
 
 def write_file(filename="", text=""):
+    """write `text` contents into `filename` file and overwrites the file if it exists"""
     with open(filename, mode="w", encoding="utf=8") as file:
         return file.write(text)
