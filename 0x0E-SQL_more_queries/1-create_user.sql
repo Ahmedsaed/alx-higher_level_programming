267737ec719291c7333e3403e1f38cb4e456bbd7
Ahmed Saed <37080003+Ahmedsaed@users.noreply.github.com>
2023-07-20 04:57:33 +0300

    revert 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- script to run msql commands
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+GRANT ALL ON *.* TO user_0d_1@localhost
