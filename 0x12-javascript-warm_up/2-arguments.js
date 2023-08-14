a090f57bd263a718bdd5c081e3c04e29e7d1237b
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-08-14 23:48:34 +0300

    Fix logic

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-if (argv.length === 0) {
+if (argv.length === 1) {
   console.log('No argument');
-} else if (argv.length === 1) {
+} else if (argv.length === 2) {
   console.log('Argument found');
-} else {
+} else if (argv.length > 2) {
   console.log('Arguments found');
 }
